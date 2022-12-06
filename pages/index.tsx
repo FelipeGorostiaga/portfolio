c3e661b0e56b75f53f1b8c90b5da67339906d849
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2022-12-06 19:52:15 -0300

    fix: fonts in documents page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,6 @@ export default function HomePage() {
         <title>Portfolio | Felipe Gorostiaga</title>
         <meta name="Portfolio" content="Felipe's portfolio website" />
         <link rel="icon" href="/rasengan.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;900&display=swap"
-              rel="stylesheet" />
       </Head>
       <Home />
     </>
