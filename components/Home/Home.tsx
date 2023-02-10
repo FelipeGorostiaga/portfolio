05c0cd0adcc30ad0588ee4fd20b75420fae59a7b
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-02-10 14:58:41 -0300

    chore: remove scss files

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,22 +1,23 @@
-import styles from './Home.module.scss';
 import Image from 'next/image';
 import Navbar from '../Layout/Navbar/Navbar';
 
 const Home = () => {
   return (
-    <main className={styles.container}>
+    <>
       <Navbar />
-      <section className={styles.imgContainer}>
-        <div className={styles.imgWrapper}>
-          <Image src="/pfp.jpg" alt="Profile picture" width={200} height={200} className={styles.pfp} />
-          {/* <Image src="/argentina.jpg" alt="Argentina flag" width={45} height={36} className={styles.arg} />*/}
+     {/* <main className="w-full h-full bg-black">
+        <section className="">
+          <div className="relative">
+            <Image src="/pfp.jpg" alt="Profile picture" width={200} height={200} className="mb-5 object-cover aspect-square rounded-full"/>
+             <Image src="/argentina.jpg" alt="Argentina flag" width={45} height={36} className="" />
+          </div>
+        </section>
+        <div className="">
+          <div className="font-bold text-xl">Felipe Gorostiaga</div>
+          <div className="">Sr. Software Engineer at Banco Galicia</div>
         </div>
-      </section>
-      <div className={styles.intro}>
-        <div className={styles.name}>Felipe Gorostiaga</div>
-        <div className={styles.career}>Software Engineer and Fullstack Developer</div>
-      </div>
-    </main>
+      </main>*/}
+    </>
   );
 };
 
--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,27 +1,52 @@
-import styles from './Navbar.module.scss';
+import Link from 'next/link';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import BedtimeIcon from '@mui/icons-material/Bedtime';
+import { useTheme } from '../../../contexts/ThemeContext';
+import { useMemo } from 'react';
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const isDark: boolean = useMemo(() => theme === 'dark', [theme]);
+
   return (
-    <nav className={styles.nav}>
-      <ul className={styles.items}>
-        <li className={styles.navItem}>
-          <a href={'#'}>HOME</a>
+    <nav className="bg-black h-3 px-14 py-10 flex flex-row items-center">
+      <ul className="flex flex-row gap-12 w-full h-full items-center">
+        <li className="font-sans text-gray-200 hover:text-gray-50">
+          <Link href={'/'}>Home</Link>
         </li>
-        <li className={styles.navItem}>
-          <a href={'#'}>PROJECTS</a>
+        <li className="font-sans text-gray-200 hover:text-gray-50">
+          <Link href={'/gallery'}>Gallery</Link>
         </li>
-        <li className={styles.navItem}>
-          <a href={'#'}>CONTACT</a>
+        <li className="font-sans text-gray-200 hover:text-gray-50">
+          <Link href={'/games'}>Games</Link>
         </li>
-        <li className={styles.navItem}>
-          <a href={'#'}>GAMES</a>
+        <li className="font-sans text-gray-200 hover:text-gray-50">
+          <Link href={'/experience'}>Experience</Link>
         </li>
-        <li className={styles.navItem}>
-          <a href={'#'}>ABOUT ME</a>
+        <li className="font-sans text-gray-200 hover:text-gray-50">
+          <Link href={'/contact'}>Contact</Link>
         </li>
       </ul>
+      {
+        isDark && (
+          <div
+            onClick={() => setTheme('dark')}
+            className="bg-[#2b2b2b] w-[40px] h-[40px] flex items-center rounded-md justify-center cursor-pointer hover:bg-gray-900">
+            <LightModeIcon style={{ color: 'white' }} fontSize="medium" />
+          </div>
+        )
+      }
+      {
+        isDark && (
+          <div
+            onClick={() => setTheme('light')}
+            className="bg-[#2b2b2b] w-[40px] h-[40px] flex items-center rounded-md justify-center cursor-pointer hover:bg-gray-900">
+            <BedtimeIcon style={{ color: 'white' }} fontSize="medium" />
+          </div>
+        )
+      }
     </nav>
   );
-}
+};
 
 export default Navbar;
