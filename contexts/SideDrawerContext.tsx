927c085a57a10b58b090eda80782d7d656ee9952
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-02-16 18:34:32 -0300

    feat: side drawer context

---
+++ contexts/SideDrawerContext.tsx
@@ -0,0 +1,30 @@
+import React, { Dispatch, SetStateAction, useContext, useState } from 'react';
+
+interface SideDrawerContextType {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const SideDrawerContext = React.createContext<SideDrawerContextType | null>(null);
+
+export const useSideDrawer = () => {
+  return useContext(SideDrawerContext) as SideDrawerContextType;
+};
+
+const SideDrawerProvider = ({ children }: { children: JSX.Element }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  
+  return (
+    <SideDrawerContext.Provider
+      value={{
+        isOpen,
+        setIsOpen,
+      }}
+    >
+      {children}
+    </SideDrawerContext.Provider>
+  );
+};
+
+export default SideDrawerProvider;
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,17 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import ThemeProvider from '../contexts/ThemeContext';
 import Navbar from '../components/Layout/Navbar/Navbar';
+import SideDrawerProvider from '../contexts/SideDrawerContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <div className='bg-gray-50 dark:bg-black min-h-screen'>
-        <Navbar/>
+      <div className="bg-gray-50 dark:bg-black min-h-screen">
+        <SideDrawerProvider>
+          <Navbar />
+        </SideDrawerProvider>
         <Component {...pageProps} />
       </div>
     </ThemeProvider>
-);
+  );
 }
