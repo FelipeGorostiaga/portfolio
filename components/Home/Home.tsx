2f3e2236858682516fb5c26f6dde83a6635aaa38
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-02-22 17:59:52 -0300

    feat: home responsive layout

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,37 +1,37 @@
 import Image from 'next/image';
 import styles from './Home.module.scss';
+import useBreakpoints from '../../hooks/useBreakpoints';
 
 const Home = () => {
+  const { xs, md } = useBreakpoints();
+  const imgSize = xs? 130 : (md? 160 : 200);
+
   return (
-    <div className="w-full max-w-5xl mx-auto pt-36 pb-36">
-     {/* <div className="flex flex-row gap-20 items-center justify-between">
-        <div className="flex flex-col gap-1">
-          <div
-            className={`${styles.animatedWording} animatedWording text-transparent leading-snug font-sans font-semibold text-6xl`}>
+    <div className="w-full max-w-5xl mx-auto pt-12 pb-16 flex flex-col md:pt-36 md:pb-24">
+      <div className="flex flex-col-reverse gap-8 items-center justify-between md:flex-row md:gap-14 md:justify-center">
+        <div className="flex flex-col gap-2 items-start justify-center">
+          <div className={`${styles.animatedWording} font-sans font-semibold text-4xl md:text-5xl lg:text-6xl`}>
             Felipe Gorostiaga
           </div>
-          <div className="text-gray-600 font-sans font-regular text-xl dark:text-gray-300">
+          <div className="text-gray-600 font-sans font-regular dark:text-gray-300 md:text-xl">
             Sr. Software Engineer at Banco Galicia
           </div>
         </div>
-        <Image className="object-cover rounded-2xl shadow-2xl" src={'/pfp.jpg'} height={250} width={250}
-               alt="profile picture" />
+        <Image className="object-cover shadow-2xl aspect-square rounded-full" src={'/pfp.jpg'} height={imgSize} width={imgSize} alt="profile picture" />
       </div>
-      <div className="w-full h-0.5 bg-gray-200 my-12 dark:bg-gray-400"></div>
-      <p
-        className="font-sans italic font-extralight text-2xl text-center text-gray-600 dark:text-gray-300 m-0">
+      <div className="w-[80%] h-0.5 bg-gray-200 my-8 dark:bg-gray-400 px-12 m-auto lg:w-full md:my-12"></div>
+      <p className="font-sans italic font-extralight text-lg text-center text-gray-600 dark:text-gray-300 m-0 px-16 md:text-xl">
         &quot;He
         who has a why to live for can
         bear almost any how&quot;
       </p>
-      <p
-        className="mt-12 font-sans text-xl font-light m-0 text-center break-normal px-16 text-gray-800 dark:text-gray-300">
+      <p className="font-sans text-base mt-12 font-light m-0 text-center break-normal px-12 text-gray-800 dark:text-gray-300 md:text-xl md:px-20">
         I am a Software Engineer born in Buenos Aires, Argentina. I am a technology enthusiast that is constantly
         learning about the world
         with the purpose of gaining new knowledge to create products and services that serve and inspire humankind. To
         create is the most noble act one can achieve, specially if something truly beautiful and useful is made. That
         is my purpose.
-      </p>*/}
+      </p>
     </div>
   );
 };
