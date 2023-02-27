8009c564ddd729e475253575e9b8721f86acae81
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-02-27 19:11:49 -0300

    fix: background image styling and remove use next-img

--- components/UI/BackgroundImage/BackgroundImage.tsx
+++ components/UI/BackgroundImage/BackgroundImage.tsx
@@ -1,17 +1,16 @@
 import styles from './BackgroundImage.module.scss';
-
-
-interface BackgroundImageProps {
-  height: string | number;
-  width: string | number;
-}
+import { useTheme } from '../../../contexts/ThemeContext';
+import Image from 'next/image';
 
 const BackgroundImage = () => {
+  const { isDark } = useTheme();
+  const backgroundImg = isDark? '/mountains.jpg' : '/snow.jpg';
+
   return (
     <div className={styles.person}>
       <div className={styles.personContainer}>
-        <img src={'/water.jpg'} className={styles.circleImg} alt=""/>
-        <img src={'/pfp-no-background.png'} className={styles.personImg} alt="profile picture"/>
+        <Image src={backgroundImg} className={styles.circleImg} alt="" width={192} height={192}/>
+        <Image src={'/pfp-no-background.png'} className={styles.personImg} alt="profile picture" width={192} height={192}/>
       </div>
     </div>
   );
