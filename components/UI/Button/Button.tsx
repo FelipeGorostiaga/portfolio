6dbefae606a010caeb531bdb0e8b1f7317de0665
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-02-28 17:39:19 -0300

    feat: button component using cva

--- components/UI/Button/Button.tsx
+++ components/UI/Button/Button.tsx
@@ -1,10 +1,35 @@
+import { cva } from 'class-variance-authority';
+
 interface ButtonProps {
   children: any;
-  variant: string;
+  intent: 'primary' | 'secondary' | 'danger';
+  fullWidth?: boolean;
+  onClick: () => void;
+  className?: string;
 }
 
-const Button = ({ children, variant }: ButtonProps) => {
-  return <button>{children}</button>;
+const buttonStyles = cva(
+  'flex items-center justify-center px-4 py-2 rounded font-base font-sans focus-outline-none cursor-pointer',
+  {
+    variants: {
+      intent: {
+        primary: 'bg-blue-600 text-gray-200 hover:bg-blue-700',
+        secondary: 'bg-transparent text-neutral-800 text-black dark:text-gray-200 dark:hover:text-white border-blue-600 border-2 hover:border-blue-700 ',
+        danger: 'bg-red-600 text-gray-100 hover:bg-red-700',
+      },
+      fullWidth: {
+        true: 'w-full',
+        false: 'w-[377px]',
+      },
+    },
+    defaultVariants: {
+      intent: 'primary',
+    },
+  },
+);
+
+const Button = ({ intent, fullWidth = false, onClick, children, className = '' }: ButtonProps) => {
+  return <button className={`${buttonStyles({ intent, fullWidth })} ${className}`} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
