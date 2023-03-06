34d8703df5dc1b9ba6ea181bb62f8ccce1eb2d20
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-06 18:02:27 -0300

    feat: form grid and text area

--- components/Contact/Form/ContactForm.tsx
+++ components/Contact/Form/ContactForm.tsx
@@ -1,14 +1,21 @@
 import styles from './ContactForm.module.scss';
 import Input from '@ui/Input';
+import useBreakpoints from '../../../hooks/useBreakpoints';
 
 const ContactForm = () => {
+  const { lg } = useBreakpoints();
   return (
     <form className={styles.form}>
       <Input type="text" label="Name" size="fullWidth" placeholder="Enter first name" />
       <Input type="text" label="Last name" size="fullWidth" placeholder="Enter last name" />
       <Input type="email" label="Email" size="fullWidth" placeholder="Enter email" />
       <Input type="phone" label="Phone" size="fullWidth" placeholder="Enter telephone (optional)" />
-      <Input type="text" label="Message" size="fullWidth" placeholder="Enter message" />
+      <Input type="text" label="Message" size="fullWidth" placeholder="Enter message"
+             containerClassName={!lg ? 'col-span-2' : ''}
+             className="resize-none"
+             multiline
+             rows={3}
+      />
     </form>
   );
 };
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -9,8 +9,7 @@ export default function ContactPage() {
         <meta name="Portfolio" content="Felipe's portfolio website" />
         <link rel="icon" href="/logo.png" />
       </Head>
-      <div className="h-full w-full flex items-start justify-center">
-        {/*<HackerText className="text-black dark:text-gray-50 text-5xl">HACKER EFFECT</HackerText>*/}
+      <div className="h-full w-full flex items-center lg:items-start justify-center pb-3">
         <Contact />
       </div>
     </>
