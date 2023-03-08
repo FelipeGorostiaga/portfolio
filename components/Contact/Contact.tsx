bc0f54c3c11e5c65b4368ea0a738dcc716a9645c
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-08 17:15:22 -0300

    feat: extract submit button to form

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -13,27 +13,23 @@ const Contact = () => {
     mx-3 sm:mx-6 md:mx-8
     dark:bg-[#050505] dark:border dark:border-gray-600 shadow-xl">
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-sans text-gray-700 dark:text-neutral-100">Let&apos;s get in <span
-          className="font-semibold text-blue-600 underline">touch.</span>
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-sans text-gray-700 dark:text-neutral-100">Let&apos;s get
+          in <span
+            className="font-semibold text-blue-600 underline">touch.</span>
         </h1>
-        <h3 className="text-sm md:text-base lg:text-xl font-sans text-gray-700 dark:text-neutral-300">Looking for a developer or just want to have a conversation on
+        <h3 className="text-sm md:text-base lg:text-xl font-sans text-gray-700 dark:text-neutral-300">Looking for a
+          developer or just want to have a conversation on
           the meaning of life? Send me a message!</h3>
       </div>
       <ContactForm />
-      <div className="flex w-full items-center justify-end pb-6 lg:pb-8">
-        {
-          sm && (<Button intent="primary" size="fullWidth" onClick={() => alert('Clicked Send')} className="mt-6 rounded-2xl">
-            Send message
-          </Button>)
-        }
-        {
-          !sm && (
-            <Button intent="primary" size="pill" onClick={() => alert('Clicked Send')} className="px-12 mt-6">
-              Send message
-            </Button>
-          )
-        }
-      </div>
+      {/*<div className="flex w-full items-center justify-end pb-6 lg:pb-8">
+        <Button intent="primary"
+                onClick={() => alert('Clicked Send')}
+                size={sm ? 'fullWidth' : 'pill'}
+                className={sm ? 'mt-6 rounded-2xl' : 'px-12 mt-6'}>
+          Send message
+        </Button>
+      </div>*/}
     </div>
   );
 };
