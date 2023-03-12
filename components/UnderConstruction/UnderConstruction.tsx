b7f75babace86dfd04c322a3b0046b245d0d0a7a
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-12 01:51:46 -0300

    feat: under construction dark mode

--- components/UnderConstruction/UnderConstruction.tsx
+++ components/UnderConstruction/UnderConstruction.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import useBreakpoints from '../../hooks/useBreakpoints';
-
+import { useTheme } from '../../contexts/ThemeContext';
 
 const UnderConstruction = () => {
   const { xs } = useBreakpoints();
+  const { isDark } = useTheme();
   return (
     <div className="flex flex-col items-start bg-transparent px-8 md:px-4">
-      <h3 className="text-neutral-800 text-xl md:text-2xl lg:text-3xl">Page under construction</h3>
-      <p className="text-neutral-800 text-sm sm:text-base ">Sorry for the inconvenience, it will be ready soon...</p>
-      <Image src='/construction-animation.gif' height={xs? 200 : 300} width={xs? 200: 300} alt="Under construction" className="self-center"/>
+      <h3 className="text-neutral-800 text-xl md:text-2xl lg:text-3xl dark:text-white">Page under construction</h3>
+      <p className="text-neutral-800 text-sm sm:text-base dark:text-gray-50">Sorry for the inconvenience, it will be ready soon...</p>
+      <Image src={isDark? '/construction-animation-dark.gif' : '/construction-animation.gif'} height={xs? 200 : 300} width={xs? 200: 300} alt="Under construction" className="self-center"/>
     </div>
   );
 }
