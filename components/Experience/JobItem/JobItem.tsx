6ca9d78137ca224f53d468c226bb2a865148c8f0
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-18 00:29:12 -0300

    feat: job item (WIP)

--- components/Experience/JobItem/JobItem.tsx
+++ components/Experience/JobItem/JobItem.tsx
@@ -2,31 +2,30 @@ import Image from 'next/image';
 import { ReactElement } from 'react';
 import { IPill } from '@ui/Pill/type';
 
-interface JobProps {
+export interface JobProps {
   title: string;
   companyName: string;
   companyLink: string;
   imgSrc: string;
   startDate: Date;
   endDate: Date;
-  current: boolean;
+  current?: boolean;
   description: string | ReactElement;
   pillList?: IPill[];
 }
 
 const JobItem = (props: JobProps) => {
   return (
-    <section>
-      <Image src={props.imgSrc} alt="" />
+    <section className='px-6 mb-16 flex flex-row'>
+      <Image src={props.imgSrc} alt="" width={100} height={100} />
       <div>
-        <h3></h3>
-        <span></span>
-        <p></p>
+        <h3 className='text-3xl'>{props.title}</h3>
+        <span>{`${props.startDate.toLocaleDateString()} - ${props.current ? 'Now' : props.endDate.toLocaleDateString()}`}</span>
+        <p>{props.description}</p>
         <div>
           {/* pill list here ---> map... */}
         </div>
       </div>
-
     </section>
   );
 };
