bc15ae9c08ff5602749ae4c0e800a324bda1963c
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-18 00:29:00 -0300

    feat: timeline component

--- components/Experience/Timeline/Timeline.tsx
+++ components/Experience/Timeline/Timeline.tsx
@@ -1,10 +1,10 @@
 const Timeline = () => {
   return (
     <div className="flex flex-col items-center justify-start">
-      <div className="relative h-[48px] aspect-square bg-blue-100">
-        <div className="absolute top-[50%] left-[50%] h-[25px] aspect-square bg-blue-700"></div>
+      <div className="relative h-[60px] aspect-square rounded-full bg-blue-100 mb-2 mt-2">
+        <div className="absolute top-[50%] left-0 right-0 m-auto translate-y-[-50%] h-[15px] aspect-square bg-blue-700 rounded-full"></div>
       </div>
-      <div className="h-full w-2 bg-blue-100"></div>
+      <div className="h-full w-2 bg-gray-200"></div>
     </div>
   );
 };
