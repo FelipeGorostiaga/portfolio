476c1bcca6d284e52b968ccc2d9214d6dc8558c7
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-20 01:26:19 -0300

    fix: timeline styling

--- components/Experience/JobItem/JobItem.tsx
+++ components/Experience/JobItem/JobItem.tsx
@@ -23,21 +23,21 @@ const JobItem = (props: JobProps) => {
   return (
     <section className="mb-16 flex flex-row bg-gray-100 px-10 py-10 rounded-xl shadow-xl w-full">
       <Image src={props.imgSrc}
-           className="h-[90px] md:h-[120px] aspect-square object-fit group-hover:opacity-80 rounded"
+             className="h-[90px] md:h-[120px] aspect-square object-fit group-hover:opacity-80 rounded"
              width={120}
              height={120}
-           alt={`logo`} />
+             alt={`logo`} />
       <div className="pl-8 flex flex-col gap-0">
         <h3 className="text-3xl font-sans font-semibold dark:text-white mb-1">{props.title}</h3>
-        <div className='mb-4'>
+        <div className="mb-4">
           <span className="text-base font-light dark:text-white">{props.companyName}</span>
-          <span className='mx-3 dark:text-white'>|</span>
+          <span className="mx-3 dark:text-white">|</span>
           <span className=" text-base font-light dark:text-white">{`${startDate} - ${endDate}`}</span>
         </div>
         <div className="text-lg font-sans text-neutral-800 dark:text-white">{props.description}</div>
-        <div className={'w-full flex flex-row justify-start items-center gap-3 mt-6'}>
+        <div className={'w-full flex flex-row justify-start items-center gap-3 mt-8'}>
           {props.pillList?.map(pill => {
-            return <Pill key={pill.label} {...pill} />
+            return <Pill key={pill.label} {...pill} />;
           })}
         </div>
       </div>
--- components/Experience/Timeline/Timeline.tsx
+++ components/Experience/Timeline/Timeline.tsx
@@ -2,7 +2,7 @@ const Timeline = ({ showBar = true }: { showBar?: boolean }) => {
   return (
     <div className="flex flex-col items-center justify-start ">
       <div className="relative h-[50px] aspect-square rounded-full bg-blue-100 bg-opacity-40 my-2">
-        <div className="absolute top-[50%] left-0 right-0 m-auto translate-y-[-50%] h-[15px] aspect-square bg-blue-500 bg-opacity-70 rounded-full"></div>
+        <div className="absolute top-[50%] left-0 right-0 m-auto translate-y-[-50%] h-[15px] aspect-square bg-blue-600 bg-opacity-60 rounded-full"></div>
       </div>
       {showBar && <div className="h-[calc(100%-60px)] w-2 bg-gray-100"></div>}
     </div>
