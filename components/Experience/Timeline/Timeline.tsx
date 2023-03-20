4cdc81b0b3644e0ed635fc8fe1a82d262cf58c5d
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-20 00:16:14 -0300

    fix: timeline height bug and don't show bar for last item

--- components/Experience/Timeline/Timeline.tsx
+++ components/Experience/Timeline/Timeline.tsx
@@ -1,10 +1,10 @@
-const Timeline = () => {
+const Timeline = ({ showBar = true }: { showBar?: boolean }) => {
   return (
-    <div className="flex flex-col items-center justify-start">
-      <div className="relative h-[60px] aspect-square rounded-full bg-blue-100 mb-2 mt-2">
-        <div className="absolute top-[50%] left-0 right-0 m-auto translate-y-[-50%] h-[15px] aspect-square bg-blue-700 rounded-full"></div>
+    <div className="flex flex-col items-center justify-start ">
+      <div className="relative h-[50px] aspect-square rounded-full bg-blue-100 bg-opacity-40 my-2">
+        <div className="absolute top-[50%] left-0 right-0 m-auto translate-y-[-50%] h-[15px] aspect-square bg-blue-500 bg-opacity-70 rounded-full"></div>
       </div>
-      <div className="h-full w-2 bg-gray-200"></div>
+      {showBar && <div className="h-[calc(100%-60px)] w-2 bg-gray-100"></div>}
     </div>
   );
 };
