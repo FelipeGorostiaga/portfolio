293ec80c3b88d8758492b8e943cd4ca0f37131d9
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-22 18:13:58 -0300

    feat: light mode home gradient bg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import Footer from '../components/Layout/Footer/Footer';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <main className="bg-gradient-to-b dark:from-black dark:to-[#020714] main-grid">
+      <main className="bg-gradient-to-b from-white via-white to-[#dce2fa] dark:from-black dark:to-[#020714] main-grid">
         <SideDrawerProvider>
           <Navbar />
         </SideDrawerProvider>
