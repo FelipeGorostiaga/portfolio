49663bdba26daaebfd4dc4ece93032145235abb9
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-24 01:30:16 -0300

    feat: pill responsive

--- components/UI/Pill/index.tsx
+++ components/UI/Pill/index.tsx
@@ -2,7 +2,7 @@ import { IPill } from '@ui/Pill/type';
 
 const Pill = ({ label, className, ...rest }: IPill) => {
   return (
-    <div className={`px-6 py-2 ${className ?? ''}`} {...rest}>{label}</div>
+    <div className={`px-4 md:px-6 py-2 ${className ?? ''}`} {...rest}>{label}</div>
   );
 };
 
