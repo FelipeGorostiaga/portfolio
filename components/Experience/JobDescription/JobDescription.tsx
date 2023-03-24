bd0a6f40bfb48bd5bc22acd6d44f055cf6fc9895
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-23 22:31:23 -0300

    fix: job item color changes

--- components/Experience/JobDescription/JobDescription.tsx
+++ components/Experience/JobDescription/JobDescription.tsx
@@ -6,9 +6,9 @@ interface Props {
 const JobDescription = ({ intro, responsibilities }: Props) => {
   return (
     <div className="flex flex-col justify-start">
-      <p className="mb-1 dark:text-neutral-300">{intro}</p>
+      <p className="mb-1 dark:text-[#adadad]">{intro}</p>
       <ul className="list-disc pl-6 pt-2">
-        {responsibilities.map((responsibility, idx) => <li key={idx} className='dark:text-neutral-300'>{responsibility}</li>)}
+        {responsibilities.map((responsibility, idx) => <li key={idx} className='dark:text-[#adadad]'>{responsibility}</li>)}
       </ul>
     </div>
   );
--- components/Experience/JobItem/JobItem.tsx
+++ components/Experience/JobItem/JobItem.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import { ReactElement, useMemo } from 'react';
 import { IPill } from '@ui/Pill/type';
 import Pill from '@ui/Pill';
+import Card from '@ui/Card/Card';
+import Link from 'next/link';
 
 export interface JobProps {
   title: string;
@@ -21,27 +23,30 @@ const JobItem = (props: JobProps) => {
   const startDate = useMemo(() => formatter.format(props.startDate), [props.startDate]);
   const endDate = useMemo(() => props.current ? 'Now' : formatter.format(props.endDate), [props]);
   return (
-    <section className="mb-16 flex flex-row bg-gray-100 px-10 py-10 rounded-xl shadow-xl w-full dark:bg-[#080808]">
-      <Image src={props.imgSrc}
-             className="h-[90px] md:h-[120px] aspect-square object-fit group-hover:opacity-80 rounded"
-             width={120}
-             height={120}
-             alt={`logo`} />
-      <div className="pl-8 flex flex-col gap-0">
-        <h3 className="text-3xl font-sans font-semibold dark:text-white mb-1">{props.title}</h3>
-        <div className="mb-4">
-          <span className="text-base font-light dark:text-white">{props.companyName}</span>
-          <span className="mx-3 dark:text-white">|</span>
-          <span className=" text-base font-light dark:text-white">{`${startDate} - ${endDate}`}</span>
+    <Card>
+      <Link href={props.companyLink}  target="_blank" rel="noopener noreferrer"
+            className="mb-16 flex flex-row bg-gray-100 px-10 py-10 rounded-xl shadow-xl w-full dark:bg-spacegray cursor-pointer">
+        <Image src={props.imgSrc}
+               className="h-[90px] md:h-[120px] aspect-square object-fit group-hover:opacity-80 rounded"
+               width={120}
+               height={120}
+               alt={`logo`} />
+        <div className="pl-8 flex flex-col gap-0">
+          <h3 className="text-3xl font-sans font-semibold dark:text-neutral-200 mb-1">{props.title}</h3>
+          <div className="mb-4">
+            <span className="text-base font-light dark:text-[#adadad]">{props.companyName}</span>
+            <span className="mx-3 dark:text-[#adadad]">|</span>
+            <span className=" text-base font-light dark:text-[#adadad]">{`${startDate} - ${endDate}`}</span>
+          </div>
+          <div className="text-lg font-sans text-neutral-800 dark:text-white">{props.description}</div>
+          <div className={'w-full flex flex-row justify-start items-center gap-3 mt-8 flex-wrap'}>
+            {props.pillList?.map(pill => {
+              return <Pill key={pill.label} {...pill} />;
+            })}
+          </div>
         </div>
-        <div className="text-lg font-sans text-neutral-800 dark:text-white">{props.description}</div>
-        <div className={'w-full flex flex-row justify-start items-center gap-3 mt-8 flex-wrap'}>
-          {props.pillList?.map(pill => {
-            return <Pill key={pill.label} {...pill} />;
-          })}
-        </div>
-      </div>
-    </section>
+      </Link>
+    </Card>
   );
 };
 
