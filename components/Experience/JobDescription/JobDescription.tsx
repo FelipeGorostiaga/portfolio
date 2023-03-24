19fb49aaa5efbb2ebda60df7a2462d079dd61f6c
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-03-24 01:30:09 -0300

    feat: job description responsive

--- components/Experience/JobDescription/JobDescription.tsx
+++ components/Experience/JobDescription/JobDescription.tsx
@@ -6,9 +6,9 @@ interface Props {
 const JobDescription = ({ intro, responsibilities }: Props) => {
   return (
     <div className="flex flex-col justify-start">
-      <p className="mb-1 dark:text-[#adadad]">{intro}</p>
-      <ul className="list-disc pl-6 pt-2">
-        {responsibilities.map((responsibility, idx) => <li key={idx} className='dark:text-[#adadad]'>{responsibility}</li>)}
+      <p className="text-xs md:text-base mb-1 dark:text-[#adadad]">{intro}</p>
+      <ul className="list-disc pl-4 md:pl-6 pt-2">
+        {responsibilities.map((responsibility, idx) => <li key={idx} className='text-xs mb-2 md:mb-0 md:text-base dark:text-[#adadad]'>{responsibility}</li>)}
       </ul>
     </div>
   );
