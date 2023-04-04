114563af6f45b900a682bc09b8d306a8042fd75c
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-04-04 18:15:43 -0300

    feat: book card dark and responsive grid

--- src/components/Gallery/Books/BookItem/BookItem.tsx
+++ src/components/Gallery/Books/BookItem/BookItem.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import StarIcon from '@mui/icons-material/Star';
+import useBreakpoints from '~/hooks/useBreakpoints';
 
 interface BookProps {
   title: string;
@@ -9,23 +10,27 @@ interface BookProps {
 }
 
 const BookItem = ({ title, author, imgUrl, score }: BookProps) => {
+  const { xs } = useBreakpoints();
   return (
-    <div className='flex flex-row items-start rounded-xl bg-neutral-100 dark:bg-neutral-800 w-[350px] aspect-video shadow-xl hover:scale-[1.01]'>
-      <div className='h-full w-[150px] relative'>
-        <Image src={imgUrl} alt='Book cover' fill className='border-inherit box-border rounded-l-xl'/>
+    <div
+      className="flex flex-row items-start rounded-xl bg-neutral-100 dark:bg-spacegray w-full min-h-[200px] aspect-video
+      shadow md:shadow-xl hover:scale-[1.01]">
+      <div className="h-full aspect-book relative">
+        <img src={imgUrl} alt="Book cover" className="rounded-l-xl h-full w-full" />
       </div>
-      <div className='flex flex-col items-start py-2 px-4 justify-between h-full'>
-        <div>
-          <h3 className='text-base font-bold'>{title}</h3>
-          <h5 className='text-sm'>{author}</h5>
+      <div className="flex flex-col items-start py-2 px-3 md:px-4 justify-between h-full w-full">
+        <div className="flex flex-col gap-0 sm:gap-1">
+          <h3 className="text-base xs:text-xl sm:text-base font-bold sm:leading-tight dark:text-neutral-200">{title}</h3>
+          <h5 className="text-xs xs:text-sm dark:text-neutral-400">{author}</h5>
         </div>
-        <div className='flex flex-row bg-black items-center justify-between rounded-xl px-3 py-1 gap-1 self-end bg-opacity-60'>
-          <StarIcon style={{ color: '#FFC728', fontSize: '18px' }} />
-          <span className='text-base text-neutral-200'>{`${score}`}</span>
+        <div
+          className="flex flex-row bg-black items-center justify-between rounded-xl px-3 py-1 gap-1 self-end bg-opacity-60">
+          <StarIcon style={{ color: '#FFC728', fontSize: xs ? '14px' : '18px' }} />
+          <span className="text-sm md:text-base text-neutral-200">{`${score}`}</span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default BookItem;
--- src/components/Gallery/Books/Books.tsx
+++ src/components/Gallery/Books/Books.tsx
@@ -3,60 +3,70 @@ import BookFilters from './BookFilters/BookFilters';
 
 const booksMocks = [
   {
-    title: 'Thus Spoke Zarathustra',
-    author: 'Friedrich Nietzsche',
-    score: 7,
-    imgUrl: '/mocks/zarathustra.jpg',
+    id: '1',
+    title: 'Behave',
+    author: 'Robert M Sapolsky',
+    score: 8.5,
+    imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/0995/9780099575061.jpg',
   },
   {
+    id: '2',
     title: 'The Lord of the Rings',
     author: 'Friedrich Nietzsche',
     score: 6,
-    imgUrl: '/mocks/zarathustra.jpg',
+    imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1404/9780140441185.jpg',
   },
   {
-    title: 'Thus Spoke Zarathustra',
-    author: 'Friedrich Nietzsche',
-    score: 7,
-    imgUrl: '/mocks/zarathustra.jpg',
-  },
-  {
-    title: 'Thus Spoke Zarathustra',
-    author: 'Friedrich Nietzsche',
-    score: 7,
-    imgUrl: '/mocks/zarathustra.jpg',
-  },
-  {
+    id: '3',
     title: 'Thus Spoke Zarathustra',
     author: 'Friedrich Nietzsche',
     score: 10,
     imgUrl: '/mocks/zarathustra.jpg',
   },
   {
+    id: '4',
+    title: 'Thus Spoke Zarathustra',
+    author: 'Friedrich Nietzsche',
+    score: 7,
+    imgUrl: '/mocks/zarathustra.jpg',
+  },
+  {
+    id: '5',
+    title: 'Thus Spoke Zarathustra',
+    author: 'Friedrich Nietzsche',
+    score: 10,
+    imgUrl: '/mocks/zarathustra.jpg',
+  },
+  {
+    id: '6',
     title: 'Thus Spoke Zarathustra',
     author: 'Friedrich Nietzsche',
     score: 2.5,
     imgUrl: '/mocks/zarathustra.jpg',
   },
   {
+    id: '7',
     title: 'Thus Spoke Zarathustra',
     author: 'Friedrich Nietzsche',
     score: 10,
     imgUrl: '/mocks/zarathustra.jpg',
   },
   {
-    title: 'Thus Spoke Zarathustra',
-    author: 'Friedrich Nietzsche',
+    id: '8',
+    title: 'El Aleph',
+    author: 'Jorge Luis Borges',
     score: 2.4,
-    imgUrl: '/mocks/zarathustra.jpg',
+    imgUrl: 'https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/3079/9780307950949.jpg',
   },
   {
+    id: '9',
     title: 'Thus Spoke Zarathustra',
     author: 'Friedrich Nietzsche',
     score: 3,
     imgUrl: '/mocks/zarathustra.jpg',
   },
   {
+    id: '10',
     title: 'Thus Spoke Zarathustra',
     author: 'Friedrich Nietzsche',
     score: 9.5,
@@ -65,17 +75,17 @@ const booksMocks = [
 ];
 
 const Books = () => {
-
   return (
-    <section className="px-20">
+    <section className="px-8 md:px-20 w-full">
       <h1
         className="text-3xl md:text-6xl text-sans font-bold mb-1 md:mb-3 text-neutral-800 dark:text-neutral-100">
         Books
       </h1>
       <BookFilters />
-      <div className='grid grid-cols-4 gap-10'>
+      <div
+        className="grid grid-cols-1 place-items-center gap-4 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 2xl:gap-10">
         {booksMocks.map(book => {
-          return <BookItem {...book} key={book.title} />;
+          return <BookItem {...book} key={book.id} />;
         })}
       </div>
     </section>
