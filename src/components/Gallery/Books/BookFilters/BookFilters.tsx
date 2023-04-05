0c0b5b55a8a382ec9e657237b9c19b96412939e4
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-04-05 16:24:40 -0300

    fix: book search dark styling

--- src/components/Gallery/Books/BookFilters/BookFilters.tsx
+++ src/components/Gallery/Books/BookFilters/BookFilters.tsx
@@ -1,6 +1,7 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { type KeyboardEvent, useState } from 'react';
 import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { useTheme } from '~/contexts/ThemeContext';
 
 type SortCriteria = 'rating' | 'year';
 type SortDirection = 'asc' | 'desc';
@@ -10,6 +11,7 @@ const BookFilters = () => {
   const [sortCriteria, setSortCriteria] = useState<SortCriteria>('rating');
   const [sortDirection, setSortDirection] = useState<SortDirection>('desc');
   const [inputFocused, setInputFocused] = useState(false);
+  const { isDark } = useTheme();
 
   const handleKeyPress = (event: KeyboardEvent) => {
     if (event.key === 'Enter') {
@@ -27,10 +29,10 @@ const BookFilters = () => {
     <div className="w-full flex flex-row items-center justify-between gap-5 my-3 py-4">
       <div
         className={`py-3 px-5 bg-neutral-50 dark:bg-neutral-800 flex flex-row items-center justify-items-start
-        gap-2 rounded-2xl sm:w-[400px] border-2 ${inputFocused ? 'border-[#1976D2]' : 'border-neutral-200'}`}>
+        gap-2 rounded-2xl sm:w-[400px] border-2 ${inputFocused ? 'border-[#1976D2]' : (isDark? 'border-neutral-700' : 'border-neutral-200')}`}>
         <input value={searchValue}
                onChange={e => setSearchValue(e.target.value)}
-               className="outline-none bg-transparent font-sans w-full caret-blue-400 text-lg text-neutral-700 group-hover:bg-red-400"
+               className="outline-none bg-transparent font-sans w-full caret-blue-400 text-lg text-neutral-700 dark:text-neutral-300"
                placeholder="Search by title, author..."
                onFocus={() => setInputFocused(true)}
                onBlur={() => setInputFocused(false)}
