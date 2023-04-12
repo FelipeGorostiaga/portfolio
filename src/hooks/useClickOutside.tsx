c6b682227e22f649fd0c0750abbf2cbaee1032ac
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-04-12 19:04:40 -0300

    feat: click outside hook

---
+++ src/hooks/useClickOutside.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+export function useOutsideAlerter(ref: any, callback: Function) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback(event);
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [callback, ref]);
+}
