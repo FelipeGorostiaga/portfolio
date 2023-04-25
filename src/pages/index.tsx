4b1d4514a9521b14af27b722f39f886086dc3fda
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-04-25 15:13:17 -0300

    chore: og meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,17 @@ export default function HomePage() {
       <Head>
         <title>Portfolio | Felipe Gorostiaga</title>
         <meta name="Portfolio" content="Felipe's portfolio website" />
+        <meta property="og:url" content="www.felipegorostiaga.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Felipe's Website"
+        />
+        <meta
+          property="og:description"
+          content="Felipe Gorostiaga | Portfolio Website"
+        />
+        <meta property="og:image" content="/logo.png" />
         <link rel="icon" href="/logo.png" />
       </Head>
       <Home />
