6d9c30c5fef913aca1f22b55cf2a951f25ee09ef
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-04-27 00:08:28 -0300

    fix: 404 logo

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,13 +14,13 @@ const NotFoundPage: NextPage = () => {
       <div
         className="h-full w-full flex flex-col items-center justify-start pt-32 px-4 md:pt-0 md:px-0 md:justify-center gap-1 md:gap-3">
         <Image
-          className="rounded-xl object-fill aspect-square dark:border-2 dark:border-neutral-400"
+          className="rounded-xl object-fill aspect-square"
           alt="logo"
           src={'/logo.png'}
           width={!md ? 100 : 60}
           height={!md ? 100 : 60}
         />
-        <div className="text-2xl md:text-3xl text-neutral-600 font-bold text-center dark:text-neutral-200">404 Page not
+        <div className="text-2xl md:text-3xl text-neutral-600 font-bold text-center dark:text-neutral-200">404 - Page not
           found
         </div>
         <div className="text-sm md:text-base text-neutral-500 text-center dark:text-neutral-400">The requested page
