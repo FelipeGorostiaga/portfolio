481f74180ca6b451a375dd5d1172ec9b85c1f070
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-04-28 17:13:43 -0300

    fix: pie chart webkit css properties

--- src/components/Skills/ SkillPieChart/SkillPieChart.tsx
+++ src/components/Skills/ SkillPieChart/SkillPieChart.tsx
@@ -9,11 +9,13 @@ interface PieChartProps {
 
 const SkillPieChart = ({ percentage, width, color = '#2563eb' }: PieChartProps) => {
   return (
-    <div className={`${styles.pie} ${styles.animate} text-sm md:text-base lg:text-lg text-neutral-900 font-semibold dark:text-neutral-200`} style={{
-      '--p': percentage,
-      '--c': color,
-      '--w': width
-    } as React.CSSProperties}>{percentage}%</div>
+    <div className={styles.wrapper}>
+      <div className={`${styles.pie} ${styles.animate} text-sm md:text-base lg:text-lg text-neutral-900 font-semibold dark:text-neutral-200`} style={{
+        '--p': percentage,
+        '--c': color,
+        '--w': width
+      } as React.CSSProperties}>{percentage}%</div>
+    </div>
   );
 };
 
