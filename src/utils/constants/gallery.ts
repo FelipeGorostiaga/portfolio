e9fc057030a720a588a88fe487f8010b0180bfcc
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-05-21 19:08:45 -0300

    feat: quotes trpc router getAll with filters

--- src/utils/constants/gallery.ts
+++ src/utils/constants/gallery.ts
@@ -1,5 +1,6 @@
 export const BOOKS_PER_PAGE = 6;
 export const MOVIES_PER_PAGE = 10;
+export const QUOTES_PER_PAGE = 15;
 
 export type SortCriteria = 'rating' | 'year';
 export type SortDirection = 'asc' | 'desc';
