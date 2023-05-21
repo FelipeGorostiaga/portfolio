d603c2d623289d831c3c6f3c99ee6b44f95ba8e3
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-05-21 19:51:17 -0300

    fix: contact form style fixes

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,7 +6,9 @@ const Contact = () => {
     pt-4 sm:pt-10 md:pt-16
     px-5 sm:px-10 md:px-12 lg:px-20
     mx-3 sm:mx-6 md:mx-8
-    dark:md:border
+     dark:sm:border
+     dark:sm:border-gray-600
+    dark:md:border-2
      dark:md:border-gray-600 md:shadow-xl">
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl md:text-3xl lg:text-4xl font-sans text-gray-700 dark:text-neutral-100">Let&apos;s get
