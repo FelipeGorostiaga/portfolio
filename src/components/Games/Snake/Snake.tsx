f717c6ce427f86d86390c64bd05a4d106cad010b
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2023-05-30 16:51:47 -0300

    feat: snake game working correctly

--- src/components/Games/Snake/Snake.tsx
+++ src/components/Games/Snake/Snake.tsx
@@ -1,11 +1,29 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { createGrid, getRoundedProperty } from '~/utils/lib/grid';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
 import HackerText from '@ui/HackerText/HackerText';
-import styles from '@components/Games/GameOfLife/GameOfLife.module.scss';
-import Controls from '@components/Games/GameOfLife/Controls/Controls';
+import useInterval from '~/hooks/useInterval';
+import styles from '../Minesweeper/Minesweeper.module.scss';
+import Image from 'next/image';
+import { useTheme } from '~/contexts/ThemeContext';
+import Button from '@ui/Button/Button';
+import ReplayIcon from '@mui/icons-material/Replay';
 
 const rows = 16;
 const cols = 16;
+
+const CANVAS_SIZE = [500, 500];
+const SNAKE_START = [
+  [8, 7],
+  [8, 8],
+];
+const APPLE_START = [8, 3];
+const SCALE = 25;
+const SPEED = 100;
+const DIRECTIONS = {
+  38: [0, -1], // up
+  40: [0, 1], // down
+  37: [-1, 0], // left
+  39: [1, 0], // right
+};
 
 interface CellProp {
   hasSnake: boolean;
@@ -13,113 +31,134 @@ interface CellProp {
   className?: string;
 }
 
-const directionsVector: { up: Direction, down: Direction, left: Direction, right: Direction } = {
-  up: [-1, 0],
-  down: [1, 0],
-  right: [0, 1],
-  left: [0, -1],
-};
-
-const GridCell = ({ hasSnake, className = '' }: CellProp) => {
+const GridCell = ({ hasSnake, hasApple, className = '' }: CellProp) => {
   return (
     <div className={`w-[18px] h-[18px] xs:w-[24px] xs:h-[24px] sm:w-[35px] sm:h-[35px] border border-neutral-400 dark:border-neutral-800  
-         ${hasSnake ? 'bg-green-600 dark:bg-green-800' : 'bg-neutral-200 dark:bg-spacegray '} transition-colors ${className}`} />
+         ${hasSnake ? 'bg-green-600 dark:bg-green-800' : hasApple ? 'bg-red-600' : 'bg-neutral-200 dark:bg-spacegray'} transition-colors ${className}`} />
   );
 };
 
-type Direction = [(0 | 1 | -1), (0 | 1 | -1)];
-type Position = [number, number];
-
-function generateFoodPosition(): [number, number] {
-  return [12, 14];
-}
-
-function generateStartingPosition(): [number, number][] {
-  return [[8, 8]];
-}
-
 export const Snake = () => {
-  const [grid, setGrid] = useState(() => createGrid(rows, cols, () => {
-  }));
-  const [foodPosition, setFoodPosition] = useState<Position>(() => generateFoodPosition());
-  const [running, setRunning] = useState(false);
-  // const [direction, setDirection] = useState<Direction>([1, 0]);
-  const [snake, setSnake] = useState<Position[]>(() => generateStartingPosition());
-  const runningRef = useRef(running);
-  const directionRef = useRef([1, 0]);
-  runningRef.current = running;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [snake, setSnake] = useState(SNAKE_START);
+  const [apple, setApple] = useState(APPLE_START);
+  const [dir, setDir] = useState([0, -1]);
+  const [speed, setSpeed] = useState<number | null>(null);
+  const [gameOver, setGameOver] = useState(false);
+  const [score, setScore] = useState(0);
 
+  const { isDark } = useTheme();
 
-  const simulate = () => {
-    if (!runningRef.current) {
-      return;
-    }
-    setSnake(snake => {
-      const direction = directionRef.current;
-      const currentPosition = snake[snake.length - 1];
-      console.log(`Previous position [${currentPosition[0]}, ${currentPosition[1]}]`);
-      console.log(`Using direction: ${direction[0]}, ${direction[1]}`);
-      const newPos: [number, number] = [currentPosition[0] + direction[0], currentPosition[1] + direction[1]];
-      console.log(`New position [${newPos[0]}, ${newPos[1]}]`);
-      return [newPos];
-    });
+  useInterval(() => gameLoop(), speed);
 
-    setTimeout(simulate, 100);
+  const endGame = () => {
+    setSpeed(null);
+    setGameOver(true);
   };
 
-  const handleKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    if (e.key === 'ArrowUp') {
-      directionRef.current = directionsVector.up;
-    } else if (e.key === 'ArrowDown') {
-      directionRef.current = directionsVector.down;
-    } else if (e.key === 'ArrowRight') {
-      directionRef.current = directionsVector.right;
-    } else if (e.key === 'ArrowLeft') {
-      directionRef.current = directionsVector.left;
+  const moveSnake = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (speed === null) {
+      startGame();
     }
+    const keyCode = e.keyCode;
+    // @ts-ignore
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
   };
 
-  const positionHasSnake = useCallback(([x, y]: Position): boolean => {
-    for (let i = 0; i < snake?.length!; i++) {
-      const currentSnakePosition = snake[i];
-      if (currentSnakePosition[0] === x && currentSnakePosition[1] === y) {
-        return true;
-      }
+  const createApple = () => apple.map((_a, i) => Math.floor(Math.random() * (CANVAS_SIZE[i] / SCALE)));
+
+  const checkCollision = (piece: number[], snk = snake) => {
+    if (
+      piece[0] * SCALE >= CANVAS_SIZE[0] ||
+      piece[0] < 0 ||
+      piece[1] * SCALE >= CANVAS_SIZE[1] ||
+      piece[1] < 0
+    )
+      return true;
+
+    for (const segment of snk) {
+      if (piece[0] === segment[0] && piece[1] === segment[1]) return true;
     }
     return false;
-  }, [snake]);
+  };
+
+  const checkAppleCollision = (newSnake: number[][]) => {
+    if (newSnake[0][0] === apple[0] && newSnake[0][1] === apple[1]) {
+      let newApple = createApple();
+      while (checkCollision(newApple, newSnake)) {
+        newApple = createApple();
+      }
+      setScore(prev => prev + 1);
+      setApple(newApple);
+      return true;
+    }
+    return false;
+  };
+
+  const gameLoop = () => {
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    snakeCopy.unshift(newSnakeHead);
+    if (checkCollision(newSnakeHead)) endGame();
+    if (!checkAppleCollision(snakeCopy)) snakeCopy.pop();
+    setSnake(snakeCopy);
+  };
+
+  const startGame = () => {
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDir([0, -1]);
+    setSpeed(SPEED);
+    setScore(0);
+    setGameOver(false);
+  };
+
+  useEffect(() => {
+    let fruit = document.getElementById('fruit') as HTMLCanvasElement;
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      if (context) {
+        context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
+        context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        const colorFill = isDark ? '#166534' : '#16a34a';
+        context.fillStyle = colorFill;
+        snake.forEach(([x, y]) => context.fillRect(x, y, 1, 1));
+        context.drawImage(fruit, apple[0], apple[1], 1, 1);
+      }
+    }
+
+  }, [snake, apple, gameOver, isDark]);
 
   // tabIndex is used so that the div is actionable and the onKeyDown event is captured
   return (
-    <div className="max-w-6xl w-full flex flex-col items-center gap-4 mx-auto focus:outline-none"
-         onKeyDown={handleKeyPress} tabIndex={-1}>
+    <div className="max-w-6xl w-full flex flex-col items-center mx-auto focus:outline-none gap-3"
+         onKeyDown={(e) => moveSnake(e)} tabIndex={-1}>
       <HackerText className="text-4xl sm:text-6xl dark:text-slate-200 text-slate-800 font-mono-game ">SNAKE</HackerText>
-      <div className={styles.gridContainer}>
-        {
-          grid.map((r, i) => r.map((col, j) => {
-              const hasApple = foodPosition[0] === i && foodPosition[1] === j;
-              const hasSnake = positionHasSnake([i, j]);
-              return (
-                <GridCell key={`${i}-${j}`}
-                          hasSnake={hasSnake}
-                          hasApple={hasApple}
-                          className={getRoundedProperty(i, j, rows, cols)} />
-              );
-            },
-          ))
-        }
+      <div className="flex flex-row items-center w-[288px] sm:w-[560px] justify-center">
+        <div className="flex flex-row items-center gap-2 mr-3">
+          <Image id="fruit" src="/apple.svg" width={30} height={30} alt="fruit" />
+          <span
+            className="text-xs sm:text-sm md:text-xl text-neutral-800 dark:text-neutral-200 font-mono font-semibold pt-0.5">{score}</span>
+        </div>
       </div>
-      <div className="w-[288px] sm:w-[560px]">
-        <Controls running={running}
-                  onPlay={() => {
-                    setRunning(prev => !prev);
-                    runningRef.current = true;
-                    simulate();
-                  }}
-                  onClear={() => {
-                  }}
-                  onRandomize={() => {
-                  }} />
+      <div className="relative">
+        <canvas
+          className="border-2 border-neutral-400 dark:border-neutral-800 rounded-lg bg-neutral-300 dark:bg-spacegray"
+          ref={canvasRef}
+          width={`${CANVAS_SIZE[0]}px`}
+          height={`${CANVAS_SIZE[1]}px`}
+        />
+        {
+          gameOver && (
+            <div className={styles.resultContainer}>
+              <div
+                className="bg-neutral-200 dark:bg-[#0c0c0c] px-6 py-5 flex flex-col justify-between items-center border border-neutral-500 dark:border-neutral-900 rounded-xl">
+                <span className="font-semibold text-neutral-700 dark:text-neutral-200 text-xl">Your score: {score}</span>
+              </div>
+            </div>
+          )
+        }
       </div>
     </div>
   );
